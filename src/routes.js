fa470ab2c42125bdb0988869ab24b783f30caa49
XQ Kuang <xuqingkuang@gmail.com>
2015-11-11 19:31:09 +0800

    * Updated to latest Babel and react-router v1.0

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Router, Route, IndexRoute} from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory'
 
 import Layout from './components/layout';
 import NotFoundPage from './pages/not-found';
 import HomePage from './pages/home'
 
+let history = createBrowserHistory()
+
 const routes = (
-  <Router>
+  <Router history={history}>
     <Route path="/" component={Layout}>
       <IndexRoute component={HomePage} />
     </Route>
